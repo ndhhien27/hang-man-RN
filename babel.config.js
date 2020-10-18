33267a0209b1ea885a7c25f09fd294079b844cfa
Nguyen Duc Hien <ndhienedu@gmail.com>
2020-10-18 16:01:07 +0700

    Initial project

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {},
+      },
+    ],
+    'jest-hoist',
+  ],
+};
